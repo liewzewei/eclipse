4b2847a98b87a1590852519d9c7be5313fb2d76e
liewzewei <68643686+liewzewei@users.noreply.github.com>
2021-07-18 17:59:28 +0800

    update
    some minor updates. problem not yet fixed

--- demo_1.java
+++ demo_1.java
@@ -5,18 +5,23 @@ public class demo_1{
 		Scanner r = new Scanner(System.in);
 		System.out.println("Please enter the number of prime numbers that you need");
 		int limit = r.nextInt();
-		int[] primeNumbers = new int[limit];
+		float[] primeNumbers = new float[limit];
 		primeNumbers[0]=2;
 		int i = 1;
 		int tryer = 2;
+		int e = 3;
 		while(i<primeNumbers.length){
-			boolean [] numberUsed;
 			for(int e = 3;;e++){
 				float number = (float)e;
-				while(tryer<number)
+				boolean usedNumbers = new boolean [number];
+				while(tryer<number) {
 					if(number / (float)tryer != 0){
-						
+						tryer++;
+						}
 					}
+				primeNumbers[i] = number;
+				e++;
+				i++;
 				}	
 			}
 		}
