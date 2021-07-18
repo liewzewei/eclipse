c4b11ec642a5d27eae5fefc57a8f09480ff837a9
liewzewei <68643686+liewzewei@users.noreply.github.com>
2021-07-18 16:21:41 +0800

    update
    Added multiple declaration of variables and loop base for prime number generator

--- demo_1.java
+++ demo_1.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
+
 public class demo_1{
-	public static void main(String[]args){}
+	public static void main(String[]args){
+		Scanner r = new Scanner(System.in);
+		System.out.println("Please enter the number of prime numbers that you need");
+		int limit = r.nextInt();
+		int[] primeNumbers = new int[limit];
+		primeNumbers[0]=2;
+		int i = 1;
+		int tryer = 2;
+		while(i<primeNumbers.length){
+			boolean [] numberUsed;
+			for(int e = 3;;e++){
+				float number = (float)e;
+				while(tryer<number)
+					if(number / (float)tryer != 0){
+						
+					}
+				}	
+			}
+		}
+	}
 }
