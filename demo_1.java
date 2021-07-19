e4e6d7b0bb3e87bb8ee3ab1861b75a3baa39c460
liewzewei <68643686+liewzewei@users.noreply.github.com>
2021-07-19 16:57:54 +0800

    Completed
    Prime Number Generator

--- demo_1.java
+++ demo_1.java
@@ -1,29 +1,35 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class demo_1{
-	public static void main(String[]args){
-		Scanner r = new Scanner(System.in);
-		System.out.println("Please enter the number of prime numbers that you need");
-		int limit = r.nextInt();
-		float[] primeNumbers = new float[limit];
-		primeNumbers[0]=2;
-		int i = 1;
-		int tryer = 2;
-		int e = 3;
-		while(i<primeNumbers.length){
-			for(int e = 3;;e++){
-				float number = (float)e;
-				boolean usedNumbers = new boolean [number];
-				while(tryer<number) {
-					if(number / (float)tryer != 0){
-						tryer++;
-						}
-					}
-				primeNumbers[i] = number;
-				e++;
-				i++;
-				}	
-			}
-		}
-	}
+	public static void main(String[] args) {
+
+        Scanner scannerVar = new Scanner(System.in);
+
+        System.out.print("Please enter the number of prime numbers that is needed: ");
+        int numberOfPrimeNumbers = scannerVar.nextInt();
+
+        int[] primeNumberArray = new int [numberOfPrimeNumbers];
+        primeNumberArray[0] = 2;
+
+        int numberOfPrimeNumbersInArray;
+        int testNumber = 3;
+        int tryNumber;
+
+        for(numberOfPrimeNumbersInArray = 1; numberOfPrimeNumbersInArray < numberOfPrimeNumbers;){
+            for(tryNumber = 2;tryNumber<=testNumber && numberOfPrimeNumbersInArray<numberOfPrimeNumbers;tryNumber++){
+                if((testNumber%tryNumber) == 0){
+                    testNumber++;
+                    break;
+                }
+                else if((testNumber/tryNumber) == 1){
+                    primeNumberArray[numberOfPrimeNumbersInArray] = testNumber;
+                    numberOfPrimeNumbersInArray++;
+                    testNumber++;
+                    break;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(primeNumberArray));
+    }
 }
